53c013a9e0d7a0b9d86ab2b85d301dd474ff5dd5
Antesh Sharma <anteshsharma642@gmail.com>
2022-01-23 00:31:26 +0530

    binary search tree: insert(), search(), inoder(), preorder() and postorder() traversal

---
+++ src/main/java/com/antesh/dsa/BinarySearchTree.java
@@ -0,0 +1,110 @@
+package com.antesh.dsa;
+
+/*
+ * https://www.javatpoint.com/binary-tree
+ * */
+public class BinarySearchTree {
+    public Node root;
+
+    public BinarySearchTree() {
+        root = null;
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.insert(50);
+        bst.insert(20);
+        bst.insert(30);
+        bst.insert(60);
+        bst.insert(10);
+        bst.insert(70);
+        bst.insert(40);
+        System.out.println("Inorder traversal of binary search tree");
+        bst.inorder(bst.root);
+
+        System.out.println("\nPreorder traversal of binary search tree");
+        bst.preorder(bst.root);
+
+        System.out.println("\nPostorder traversal of binary search tree");
+        bst.postorder(bst.root);
+
+        System.out.print(bst.search(40) ? "\n40 found in BST" : "\n40 not found in BST");
+        System.out.print(bst.search(90) ? "\n90 found in BST" : "\n90 not found in BST");
+    }
+
+    public void insert(int data) {
+        root = insert(root, data);
+    }
+
+    public Node insert(Node node, int data) {
+        if (node == null) {
+            return new Node(data);
+        }
+
+        if (data < node.data) {
+            node.left = insert(node.left, data);
+        } else if (data > node.data) {
+            node.right = insert(node.right, data);
+        }
+
+        return node;
+    }
+
+    public boolean search(int key) {
+        Node current = root;
+
+        while (current.data != key) {
+            if (key < current.data) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+            if (current == null) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void inorder(Node node) {
+        if (node != null) {
+            inorder(node.left);
+            node.display();
+            inorder(node.right);
+        }
+    }
+
+    public void preorder(Node node) {
+        if (node != null) {
+            node.display();
+            preorder(node.left);
+            preorder(node.right);
+        }
+    }
+
+    public void postorder(Node node) {
+        if (node != null) {
+            postorder(node.right);
+            postorder(node.left);
+            node.display();
+
+        }
+    }
+
+    public static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+
+        public void display() {
+            System.out.print(data + " ");
+        }
+    }
+}
